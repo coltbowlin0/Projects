b662127bac91a5f31aadbb004efacfbcdb4849ed
coltbowlin0 <coltbowlin0@gmail.com>
2026-03-28 08:21:55 -0400

    cleaning up

--- FlappyBird/main.lua
+++ FlappyBird/main.lua
@@ -8,22 +8,9 @@ function love.load()
     logo = love.graphics.newVideo('vids/Logo-Animation.ogv')
     logo_state = "Playing"
     logo:play()
-    obsticles = {}
 
 end
-timer = 0
-function obsticle_S()
-    obsticle = {
-    color = 2.52, 2.11, 3,
-    w = 50,
-    h = 1000,
-    gapY = VH / 2,
-    mode = "fill"
-    }
-    if timer >= 0.99 then
-        table.insert(obsticles, obsticle)
-    end
-end
+
 function love.resize(w, h)
     push:resize(w, h)
 end
@@ -35,10 +22,7 @@ function love.update(dt)
         logo_state = "Ready"
     end
     if logo_state == "Ready" then
-        timer = timer + dt
-        if timer >= 1 then
-            timer = 0
-        end
+
     end
 end
 
@@ -47,10 +31,6 @@ function love.draw()
         if logo_state == "Playing" then
             love.graphics.draw(logo, 0, 0)
         elseif logo_state == "Ready" then
-            for obsticle in ipairs(obsticles) do
-                love.grahpics.color = obsticle.color
-                love.graphics.rectangle(obsticle.mode, obsticle.x, obsticle.y, obsticle.width, obsticle.height)
-            end
         end
     push:finish()
 end

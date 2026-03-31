9508160dee9344db868c7f326b42df35a3226a5b
coltbowlin0 <coltbowlin0@gmail.com>
2026-03-31 08:08:20 -0400

    tyring something

--- dinosaur-game.io/dinosaur-game.io/themes/dinosaur-game.io_v4/resources/js/ga_tracking.js
+++
@@ -1,8 +0,0 @@
-
-function sendEventToGA(eventName, params) {
-    if (typeof gtag === 'function') {
-        gtag('event', eventName, params);
-    } else {
-        console.warn('GA4 is not loaded. Event not sent.');
-    }
-}

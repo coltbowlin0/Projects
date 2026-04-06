7e82a45a569c00791f082d6c575752000ac94598
coltbowlin0 <coltbowlin0@gmail.com>
2026-04-05 20:18:55 -0400

    updating lua

---
+++ bouncing_ball/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.title = "Testing"
+    t.window.vsync = true
+    t.window.resizable = true
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+    t.version = "11.5"
+end

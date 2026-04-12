0b431e818926d5d16d322ead4cd1cfd69b95de1e
coltbowlin0 <coltbowlin0@gmail.com>
2026-04-11 20:23:14 -0400

    ive relearned

--- bouncing_ball/main.lua
+++ bouncing_ball/main.lua
@@ -1,7 +1,7 @@
 -- Resolution setup
 local WW, WH = love.window.getDesktopDimensions()
 WW, WH = WW * 0.8, WH * 0.8
-local VW, VH = 500, 500
+local VW, VH = WW, WH
 
 -- Animation variables
 local logo_sheet, logo_quad
@@ -27,16 +27,15 @@ function love.load()
         logo_state = "Ready"
         print("Logo sheet failed to load: skipping to Ready.")
     end
-
-    if logo_state == "Ready" then
-        ball = {
-            x = VW / 2,
-            y = VH / 2,
-            radius = 20,
-            speedX = 200,
-            speedY = 150
-        }
-    end
+    ball = {
+        x = VW / 2;
+        y = VH / 2;
+        radius = 50;
+        speedx = 100;
+        speedy = 100;
+        color = 1;
+        COLOR = {1, 1, 1}
+    }
 end
 
 function love.resize(w, h)
@@ -64,14 +63,45 @@ function love.update(dt)
                 logo_state = "Ready"
             end
         end
+    elseif logo_state == "Ready" then
+        ball.x = ball.x + ball.speedx * dt
+        ball.y = ball.y + ball.speedy * dt
+
+        if ball.x + ball.radius >= VW then
+            ball.speedx = -ball.speedx
+            ball.color = ball.color + 1
+        elseif ball.x - ball.radius <= 0 then
+            ball.speedx = -ball.speedx
+            ball.color = ball.color + 1
+        end
+
+        if ball.y + ball.radius >= VH then
+            ball.speedy = -ball.speedy
+            ball.color = ball.color + 1
+        elseif ball.y - ball.radius <= 0 then
+            ball.speedy = -ball.speedy
+            ball.color = ball.color + 1
+        end
+
+        if ball.color > 3 then
+            ball.color = 1
+        end
+        if ball.color == 1 then
+            ball.COLOR = {1, 0, 0}
+        elseif ball.color == 2 then
+            ball.COLOR = {1, 1, 1}
+        elseif ball.color == 3 then 
+            ball.COLOR = {0, 0, 1}
+        end
     end
 end
 
 function love.draw()
     push:start()
         if logo_state == "Playing" and logo_sheet then
-            love.graphics.draw(logo_sheet, logo_quad, 130, 130)
+            love.graphics.draw(logo_sheet, logo_quad, VW / 2, VH / 2, 0, 2, 2, frameWidth / 2, frameHeight / 2)
         elseif logo_state == "Ready" then
+            love.graphics.setColor(ball.COLOR)
             love.graphics.circle("fill", ball.x, ball.y, ball.radius)
         end
     push:finish()
